81c4c9e437ab829a63eae141debcd32b1357652b
splashley <ashleyjlhynes@gmail.com>
2021-03-31 23:57:35 -0400

    setting up react app

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div id="intro">
+        <div id="intro-left"></div>
+        <div id="intro-right"></div>
+      </div>
+      <div id="text-fe">& I want to be your next frontend developer</div>
+      <div id="work-for-you">
+        <div id="work-for-you-left"></div>
+        <div id="work-for-you-right"></div>
+      </div>
+      <div id="hire-me"></div>
+      <div id="thank-you"></div>
+      <footer>Static page created by Ashley Hynes using React.js</footer>
+    </div>
+  );
+}
+
+export default App;
